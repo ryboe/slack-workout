b29b44ed4acc5276efe7768647eda98b90571bbc
y0ssar1an <rboehning@omaze.com>
2015-10-11 08:00:56 -0700

    Refactor into separate channel package

---
+++ bot.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+
+	"channel"
+)
+
+const slackbotURL = "https://%s.slack.com/services/hooks/slackbot?%s"
+
+var botToken = os.Getenv("SLACK_BOT_TOKEN")
+
+func main() {
+	if botToken == "" {
+		log.Fatal("SLACK_BOT_TOKEN not set")
+	}
+
+	ch, err := channel.New("api-test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(ch)
+
+	// nextMember := make(chan string)
+	// go randomMember(ch, nextMember)
+}
+
+// // DEBUG
+// for i := 0; i < 5; i++ {
+// 	fmt.Println(<-nextMember)
+// }
+
+// v := url.Values{}
+// v.Add("token", botToken)
+// v.Add("channel", channel)
+
+// body := bytes.NewBufferString("hello world")
+// req, err := http.NewRequest("POST", slackbotURL+v.Encode(), body)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+
+// client := &http.Client{}
+// resp, err := client.Do(req)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// defer resp.Body.Close()
+
+// TODO: what is the format of the response?
+
+// apiToken := os.Getenv("SLACK_URL_TOKEN")
+// resp, err := http.Get(apiUrl + token)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// defer resp.Body.Close()
+
+// var js interface{}
+// err = json.NewDecoder(resp.Body).Decode(&js)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+
+// prettyJs, err := json.MarshalIndent(&js, "", "    ")
+// if err != nil {
+// 	log.Fatal(err)
+// }
+
+// fmt.Println(string(prettyJs))
+
+// func getChannel(channel string) (Channel, error) {
+// 	var emptyChannel Channel
+
+// 	qsp := map[string]string{
+// 		"channel": channel,
+// 		"token":   apiToken,
+// 	}
+// 	listURL := makeURL(apiURL, "channels.list", qsp)
+// 	resp, err := http.Get(listURL)
+// 	if err != nil {
+// 		return emptyChannel, err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	var cl ChannelListResponse
+// 	err = json.NewDecoder(resp.Body).Decode(&cl)
+// 	if err != nil {
+// 		return emptyChannel, err
+// 	}
+
+// 	if cl.Ok != true {
+// 		// DEBUG
+// 		fmt.Println(cl)
+
+// 		return emptyChannel, errors.New("failed to get channel list from Slack API")
+// 	}
+
+// 	for _, ch := range cl.Channels {
+// 		if ch.Name == channel {
+// 			return ch, nil
+// 		}
+// 	}
+
+// 	return emptyChannel, fmt.Errorf("no channel with name %q on team %q", channel, team)
+// }
+
+func prettyJSON(js interface{}) (string, error) {
+	prettyJs, err := json.MarshalIndent(&js, "", "    ")
+	if err != nil {
+		return "", err
+	}
+	return string(prettyJs), nil
+}
+
+// func makeURL(slackURL, method string, qsp map[string]string) string {
+// 	qs := queryString(qsp)
+// 	return fmt.Sprintf(apiURL, team, method, qs)
+// }
+
+// func queryString(qsp map[string]string) string {
+// 	vals := url.Values{}
+// 	for k, v := range qsp {
+// 		vals.Add(k, v)
+// 	}
+// 	return vals.Encode()
+// }
+
+func randomMember(ch Channel, nextMember chan string) {
+	var err error
+	for {
+		err = ch.UpdateMembers()
+		if err != nil {
+			log.Println(err)
+		}
+		i := rand.Intn(len(ch.Members))
+		nextMember <- ch.Members[i]
+	}
+}
+
+// func updateMembers(members []string, channelURL string) ([]string, bool) {
+// 	// If this is the first time running, members will be nil
+// 	noMembers := members == nil
+
+// 	resp, err := http.Get(channelURL)
+// 	if err != nil {
+// 		handleAPIError(noMembers, err)
+// 		return members, true
+// 	}
+// 	defer resp.Body.Close()
+
+// 	body := SlackResponse{}
+// 	err = json.NewDecoder(resp.Body).Decode(&body)
+// 	if err != nil {
+// 		handleAPIError(noMembers, err)
+// 		return members, true
+// 	}
+
+// 	if !body.Ok {
+// 		handleAPIError(noMembers, errors.New("Slack API returned error message"))
+// 		return members, true
+// 	}
+
+// 	return body.Channel.Members, false
+// }
---
+++ internal/channel/channel.go
@@ -0,0 +1,120 @@
+// TODO: write package comment
+package channel
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+const (
+	team   = "monkeytacos"
+	apiURL = "https://%s.slack.com/api/%s?%s"
+)
+
+var apiToken string
+
+type channelListResponse struct {
+	Ok       bool      `json:"ok"`
+	Channels []Channel `json:"channels"`
+}
+
+type channelResponse struct {
+	Ok      bool    `json:"ok"`
+	Channel Channel `json:"channel"`
+}
+
+type Channel struct {
+	Id      string   `json:"id"`
+	Name    string   `json:"name"`
+	Members []string `json:"members"`
+}
+
+func init() {
+	apiToken = os.Getenv("SLACK_API_TOKEN")
+	if apiToken == "" {
+		log.Fatal("SLACK_API_TOKEN not set")
+	}
+}
+
+func New(name string) (Channel, error) {
+	var emptyChannel Channel
+
+	qsp := map[string]string{
+		"channel": name,
+		"token":   apiToken,
+	}
+	listURL := makeURL(apiURL, "channels.list", qsp)
+	resp, err := http.Get(listURL)
+	if err != nil {
+		return emptyChannel, err
+	}
+	defer resp.Body.Close()
+
+	var cl channelListResponse
+	err = json.NewDecoder(resp.Body).Decode(&cl)
+	if err != nil {
+		return emptyChannel, err
+	}
+
+	if cl.Ok != true {
+		return emptyChannel, errors.New("failed to get channel list from Slack API")
+	}
+
+	for _, ch := range cl.Channels {
+		if ch.Name == name {
+			return ch, nil
+		}
+	}
+
+	return emptyChannel, fmt.Errorf("no channel with name %q on team %q", name, team)
+}
+
+func (ch Channel) String() string {
+	return fmt.Sprintf("Channel{Id: %s, Name: %s, Members: %v}", ch.Id, ch.Name, ch.Members)
+}
+
+// TODO: should this be pointer or value?
+func (ch *Channel) UpdateMembers() error {
+	qsp := map[string]string{
+		"channel": ch.Id,
+		"token":   apiToken,
+	}
+	channelURL := makeURL(apiURL, "channel.info", qsp)
+
+	resp, err := http.Get(channelURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	cr := channelResponse{}
+	err = json.NewDecoder(resp.Body).Decode(&cr)
+	if err != nil {
+		return err
+	}
+
+	if !cr.Ok {
+		return errors.New("Slack API returned an error")
+	}
+
+	ch.Members = cr.Channel.Members
+	return nil
+}
+
+func makeURL(slackURL, method string, qsp map[string]string) string {
+	qs := queryString(qsp)
+	return fmt.Sprintf(apiURL, team, method, qs)
+}
+
+func queryString(qsp map[string]string) string {
+	vals := url.Values{}
+	for k, v := range qsp {
+		vals.Add(k, v)
+	}
+	return vals.Encode()
+}
