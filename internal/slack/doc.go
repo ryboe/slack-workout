aa6f68627a8f9a09ff86d17a1a8c5e1c373aba8b
y0ssar1an <rboehning@omaze.com>
2015-10-17 11:59:00 -0700

    Add doc.go file as place to put package comment

---
+++ internal/slack/doc.go
@@ -0,0 +1,4 @@
+// Package slack implements the Channel type, for getting information about
+// Slack channels, the User type, for getting the names of Slack members,
+// and the Bot type, for posting push-up messages.
+package slack
